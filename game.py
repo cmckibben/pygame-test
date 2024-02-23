90eccb358c13754b5dc67a5c70106467fe98f304
cmckibben <151051216+cmckibben@users.noreply.github.com>
2024-02-23 15:35:09 +0000

    works!!!

--- game.py
+++ game.py
@@ -40,3 +40,16 @@ while running:
         # Did the user click the window close button? If so, stop the loop.
         elif event.type == QUIT:
             running = False
+            # Fill the screen with white
+    # Fill the screen with white
+    screen.fill((255, 255, 255))
+
+    # Create a surface and pass in a tuple containing its length and width
+    surf = pygame.Surface((50, 50))
+
+    # Give the surface a color to separate it from the background
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
+    # This line says "Draw surf onto the screen at the center"
+    screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    pygame.display.flip()
